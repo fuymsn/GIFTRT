89304102c82f087ea158e2f71c5436d5d5425814
fuymsn <fuymsn@hotmail.com>
2016-08-31 03:43:22 +0700

    更新视频连接

--- src/app/components/VideoPlayer.js
+++ src/app/components/VideoPlayer.js
@@ -6,6 +6,14 @@ const styles = {
         overflow: "hidden",
         position: "relative"
     },
+
+    videoPlayerInner: {
+        height: '240px',
+        zIndex:0,
+        position: 'absolute',
+        top: '0px',
+        left: '0px'
+    },
     
     videoImage: {
         height: "auto",
@@ -13,7 +21,8 @@ const styles = {
         position: "relative",
         transform: "translateY(-50%)",
         top: "50%",
-        left: "0px"
+        left: "0px",
+        zIndex: 10
     }
 }
 
@@ -21,7 +30,11 @@ class VideoPlayer extends Component{
     
     render() {
         return (
-            <div style={ styles.videoPlayer } data-src="http://hls.wspull.bn.netease.com/pushstation/49078526/playlist.m3u8">
+            
+            <div style={ styles.videoPlayer }>
+                <video id="live" controls style={ styles.videoPlayerInner }>
+                    <source src="http://103.12.152.102/v44184064-49756942__18185_hls/playlist.m3u8" type="video/mp4" />
+                </video>
                 <img style={ styles.videoImage } src="http://bobo-public.nosdn.127.net/anchor_1472109586899_58602517.jpg" />
             </div>
         )
